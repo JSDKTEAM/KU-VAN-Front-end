0f60288f41243425b445432c6f254e39bcf1df90
JSDKTEAM <jetsadakorn1@windowslive.com>
2019-04-11 01:44:05 +0700

    first-commit2

---
+++ webpack.config.dev.js
@@ -0,0 +1,34 @@
+const webpack = require('webpack');
+
+module.exports = {
+    devServer: {
+        contentBase: __dirname + '/src',
+        compress: true,
+        historyApiFallback: true,
+        hot: true,
+        port: 9000
+    },
+    entry: [
+        './src/index.js'
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            }
+        ]
+    },
+    output: {
+        filename: 'bundle.js',
+        path: __dirname + '/dist',
+        publicPath: '/'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    target: 'web'
+};
