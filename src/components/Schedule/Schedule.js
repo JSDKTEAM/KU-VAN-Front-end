08e4cd7cc3653ded55ed52ef190995db2a6dc5fc
MadeenaWangjaem <41312312+MadeenaWangjaem@users.noreply.github.com>
2019-04-17 15:10:58 +0700

    Schedule & FormDialog

--- src/components/Schedule/Schedule.js
+++ src/components/Schedule/Schedule.js
@@ -22,12 +22,16 @@ import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import Fab from '@material-ui/core/Fab';
 
+import FormDialog from '../UI/FormDialog/FormDialog';
+import TextField from '@material-ui/core/TextField';
+import DialogContentText from '@material-ui/core/DialogContentText';
+
 const styles = theme => ({
   root: {
     ...theme.mixins.gutters(),
     paddingTop: theme.spacing.unit * 2,
     paddingBottom: theme.spacing.unit * 2,
-    borderRadius : "15px",
+    borderRadius: "15px",
     boxShadow: `0 5px 10px 0 rgba(0, 0, 0, 0.13)`,
   },
   margin: {
@@ -36,7 +40,7 @@ const styles = theme => ({
   icon: {
     fontSize: 20,
   },
-  iconLG : {
+  iconLG: {
     fontSize: 60,
   },
   text: {
@@ -44,151 +48,157 @@ const styles = theme => ({
     fontSize: 15,
     fontFamily: [
       'Kanit'
-  ],
+    ],
   }
 });
 
-const Schedule = (props) => {
-  console.log(props);
-  const { classes } = props;
-  return (
-    <div className={classes.card}>
-      <ListItem alignItems="flex-start">
-        <Paper style={{ display: 'inline-flex', width: '100%' }}
-          className={classes.root}>
+class Schedule extends React.Component {
+  state = {
+    open: false,
+  };
+
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+  render() {
+    const { classes } = this.props;
+    const dialogChildren = (
+      <div>
+          <DialogContentText>
+              <Grid item xs container direction="column" spacing={16}>
+                <Grid item xs >
+                    สถานีต้นสาย : บางเขน
+                </Grid>
+                <Grid item xs >
+                   เวลารถ : 8.30-9.00 น.
+                </Grid>
+                <Grid item xs>
+                    ชื่อ : มาดีนะฮ์  หวังแจ่ม
+                  </Grid>
+                <Grid item xs>
+                    เพิ่มจำนวนที่นั่ง : 
+                    <AddIcon />
+                </Grid>
+              </Grid>
+            </DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="ระบุปลายทางของท่าน"
+              fullWidth
+            />
+      </div>
+
+    );
+    return (
+      <div className={classes.card}>
+        <ListItem alignItems="flex-start">
+          <Paper style={{ display: 'inline-flex', width: '100%' }}
+            className={classes.root}>
             <Grid container>
-          <Media query="(max-width: 599px)" className={classes.grid}>
-            {matches =>
-              matches ? (
-                <>
-                  {/* <Grid item xs={4}>
-                    <Paper className={classes.paper}>
-                      เวลารถออก: {props.time_out}
-                          </Paper>
-                  </Grid>
-                  <Grid item xs={4}>
-                    <Paper className={classes.paper}>
-                      จำนวนผู้จองที่นั่ง : {props.count_seat}
-                          </Paper>
-                  </Grid>
-                  <Grid item xs={4}>
-                    <Paper className={classes.paper}>
-                      <SvgIcon {...props}
-                        onClick={props.booking}>
-                        <path d="M4 6H2v14c0 1.1.9 2 2 2h14v-2H4V6zm16-4H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-1 9h-4v4h-2v-4H9V9h4V5h2v4h4v2z" />
-                      </SvgIcon>
-                          </Paper>
-                  </Grid>
-                  <Grid item xs={12}>
-                    <Paper className={classes.paper}>
-                        ทะเบียน : {props.license_plate + ':' + props.province}
-                          </Paper>
-                  </Grid> */}
-                  {/* <Typography variant="fullWidth" p={2} color="primary"
-                    className={classes.text}>
-                    Time out: {props.time_out}, Count : {props.count_seat} ทะเบียน : {props.license_plate + ':' + props.province}
-                  </Typography>
-                  <SvgIcon {...props}
-                    onClick={props.booking}>
-                    <path d="M4 6H2v14c0 1.1.9 2 2 2h14v-2H4V6zm16-4H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-1 9h-4v4h-2v-4H9V9h4V5h2v4h4v2z" />
-                  </SvgIcon> */}
+              <Media query="(max-width: 599px)" className={classes.grid}>
+                {matches =>
+                  matches ? (
+                    <>
+                      
                       <Grid item xs={2}>
-                          <Grid item xs container direction="column" justify="center" alignItems="center">
-                              <Grid item xs>
-                                  <ScheduleIcon  style={{color:"#09a76a",fontSize:40}}/>
-                              </Grid>
-                            </Grid>
-                       </Grid>
+                        <Grid item xs container direction="column" justify="center" alignItems="center">
+                          <Grid item xs>
+                            <ScheduleIcon style={{ color: "#09a76a", fontSize: 40 }} />
+                          </Grid>
+                        </Grid>
+                      </Grid>
                       <Grid item xs={10}>
                         <Grid item xs container direction="column" justify="center" alignItems="center">
                           <Grid item xs>
                             <Typography gutterBottom variant="h6">
-                              เวลารถออก : {props.time_out} น. 
+                              เวลารถออก : {this.props.time_out} น.
                             </Typography>
-                            <Typography gutterBottom variant="subtitle1">ป้ายทะเบียนรถ : {props.license_plate + ' ' + props.province}</Typography>
+                            <Typography gutterBottom variant="subtitle1">ป้ายทะเบียนรถ : {this.props.license_plate + ' ' + this.props.province}</Typography>
                           </Grid>
                         </Grid>
                       </Grid>
                       <Grid item xs={12} >
-                        <Grid item xs container  direction="row" justify="center" alignItems="center"> 
+                        <Grid item xs container direction="row" justify="center" alignItems="center">
                           <Grid item>
-                            <PersonIcon  className={classes.iconLG} style={{color:"#09a76a"}}/>
+                            <PersonIcon className={classes.iconLG} style={{ color: "#09a76a" }} />
                           </Grid>
                           <Grid item >
-                            <Typography variant="subtitle1">{props.count_seat} / {props.number_of_seats} คน</Typography>
+                            <Typography variant="subtitle1">{this.props.count_seat} / {this.props.number_of_seats} คน</Typography>
                           </Grid>
                           <Grid item >
-                            <Fab variant="extended" size="large" color="primary" style={{width:"150px"}} className={classes.margin}><AddIcon/> จอง</Fab>
+                          <FormDialog
+                            handleClickOpen = {this.handleClickOpen}
+                            handleClose = {this.handleClose}
+                            open = {this.state.open}
+                            onClose = {this.handleClose}
+                            nameOpenButton = "จอง"
+                            nameOkButton = "ยืนยัน"
+                            nameCancleButton = "ยกเลิก"
+                          >
+                          {dialogChildren}
+                          </FormDialog>
+                            {/* <Fab variant="extended" size="large" color="primary" style={{width:"150px"}} className={classes.margin}><AddIcon/> จอง</Fab> */}
                           </Grid>
                         </Grid>
                       </Grid>
-                      {/* <Grid item xs={12}  >
-                        <Grid item xs container direction="column" justify="center" alignItems="center" > 
-                        <Grid item xs >
-                          <Fab variant="extended" size="large" color="primary" style={{width:"150px"}} className={classes.margin}><AddIcon/> จอง</Fab>
-                          <Fab variant="contained" size="large" color="secondary" className={classes.margin}><EyeIcon/></Fab>
-                        </Grid>
-                        </Grid>
-                    </Grid> */}
-                </>
+                    </>
 
-              ) : (
-                  <>
-                   
-                      {/* <Typography variant="fullWidth"  color="#000"
-                        className={classes.text}>
-                        Time out: {props.time_out}, Count : {props.count_seat} ทะเบียน : {props.license_plate + ':' + props.province}
-                      </Typography>
-                      <SvgIcon {...props}
-                        onClick={props.booking}>
-                        <path d="M4 6H2v14c0 1.1.9 2 2 2h14v-2H4V6zm16-4H8c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm-1 9h-4v4h-2v-4H9V9h4V5h2v4h4v2z" />
-                      </SvgIcon> */}
-                       <Grid item xs={1}>
+                  ) : (
+                      <>
+                        <Grid item xs={1}>
                           <Grid item xs container direction="column" justify="center" alignItems="center">
-                              <Grid item xs>
-                                  <ScheduleIcon className={classes.iconLG} style={{color:"#09a76a"}}/>
-                              </Grid>
+                            <Grid item xs>
+                              <ScheduleIcon className={classes.iconLG} style={{ color: "#09a76a" }} />
                             </Grid>
-                       </Grid>
-                      <Grid item xs={4}>
-                        <Grid item xs container direction="column" >
-                          <Grid item xs>
-                            <Typography gutterBottom variant="h4">
-                              เวลารถออก : {props.time_out} น. 
+                          </Grid>
+                        </Grid>
+                        <Grid item xs={4}>
+                          <Grid item xs container direction="column" >
+                            <Grid item xs>
+                              <Typography gutterBottom variant="h4">
+                                เวลารถออก : {this.props.time_out} น.
                             </Typography>
-                            <Typography gutterBottom variant="subtitle1">ป้ายทะเบียนรถ : {props.license_plate + ' ' + props.province}</Typography>
+                              <Typography gutterBottom variant="subtitle1">ป้ายทะเบียนรถ : {this.props.license_plate + ' ' + this.props.province}</Typography>
+                            </Grid>
                           </Grid>
                         </Grid>
-                      </Grid>
-                      <Grid item xs={2}  style={{borderLeft:"2px",borderLeftStyle: 'solid', borderLeftColor: '#000'}}>
-                        <Grid item xs container  direction="row" justify="center" alignItems="center"> 
-                          <Grid item>
-                            <PersonIcon  className={classes.iconLG} style={{color:"#09a76a"}}/>
+                        <Grid item xs={2} style={{ borderLeft: "2px", borderLeftStyle: 'solid', borderLeftColor: '#000' }}>
+                          <Grid item xs container direction="row" justify="center" alignItems="center">
+                            <Grid item>
+                              <PersonIcon className={classes.iconLG} style={{ color: "#09a76a" }} />
+                            </Grid>
+                            <Grid item >
+                              <Typography variant="subtitle1">{this.props.count_seat} / {this.props.number_of_seats} คน</Typography>
+                            </Grid>
                           </Grid>
-                          <Grid item >
-                            <Typography variant="subtitle1">{props.count_seat} / {props.number_of_seats} คน</Typography>
+                        </Grid>
+                        <Grid item xs={4}  >
+                          <Grid item xs container direction="column" justify="center" alignItems="center" spacing={24}>
+                            <Grid item xs >
+                              <Fab variant="extended" size="large" color="primary" style={{ width: "150px" }} className={classes.margin}><AddIcon /> จอง</Fab>
+                              <Fab variant="contained" size="large" color="secondary" className={classes.margin}><EyeIcon /></Fab>
+                            </Grid>
                           </Grid>
                         </Grid>
-                      </Grid>
-                      <Grid item xs={4}  >
-                        <Grid item xs container direction="column" justify="center" alignItems="center" spacing={24}> 
-                        <Grid item xs >
-                          <Fab variant="extended" size="large" color="primary" style={{width:"150px"}} className={classes.margin}><AddIcon/> จอง</Fab>
-                          <Fab variant="contained" size="large" color="secondary" className={classes.margin}><EyeIcon/></Fab>
-                        </Grid>
-                        </Grid>
-                      </Grid>
-                    
 
-                  </>
-                )
-            }
-          </Media>
-          </Grid>
-        </Paper>
-      </ListItem>
-    </div>
-  )
+
+                      </>
+                    )
+                }
+              </Media>
+            </Grid>
+          </Paper>
+        </ListItem>
+      </div>
+    )
+  }
+
 }
 
 Schedule.propTypes = {
---
+++ src/components/UI/FormDialog/FormDialog.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Fab from '@material-ui/core/Fab';
+
+import { withStyles } from '@material-ui/core/styles';
+import AddIcon from '@material-ui/icons/Add';
+
+const styles = theme => ({
+  root: {
+    fontSize: 15,
+    fontFamily: [
+      'Kanit'
+    ],
+  },
+  margin: {
+    margin: theme.spacing.unit,
+  },
+});
+
+class FormDialog extends React.Component  {
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        <Fab 
+          variant="extended" 
+          size="large" color="primary" 
+          style={{width:"150px"}} 
+          className={classes.margin} 
+          onClick={this.props.handleClickOpen}
+            >
+          <AddIcon/> 
+          {this.props.nameButton}
+        </Fab>
+        <Dialog
+          open={this.props.open}
+          onClose={this.props.handleClose}
+          aria-labelledby="form-dialog-title"
+        >
+          <DialogTitle id="form-dialog-title">ข้อมูลการจอง</DialogTitle>
+          <DialogContent>
+
+            {this.props.children}
+            
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.props.handleClose} color="primary">
+              {this.props.nameCancleButton}
+            </Button>
+            <Button onClick={this.props.handleClose} color="primary">
+              {this.props.nameCancleButton}
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  }
+    
+  
+}
+
+
+export default withStyles(styles)(FormDialog);
